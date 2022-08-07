c501dc33735362485a4825c1e6637ab5874abea0
Xzn <xuzhennan0205@163.com>
2022-08-07 19:41:54 +0800

    feature create new program

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import RedBlackTree from './components/RedBlackTree';
+import Header from './views/layout/components/Header';
+// import InputField from './components/InputField';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* <InputField /> */}
+      <RedBlackTree />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/views/layout/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+const Header = () => {
+  return <span className='heading'>
+    <span className='heading-red'>Red</span>-
+    <span className='heading-black'>Black</span>&nbsp;&nbsp;
+    <span className='heading-tree'>Tree</span>
+  </span>;
+};
+
+export default Header;
