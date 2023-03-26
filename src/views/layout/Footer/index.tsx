cca53473b911f53dc89390dce07df60957310b17
Xzn <xuzhennan0205@163.com>
2023-03-26 21:34:32 +0800

    feat: 修改部分样式

--- src/views/layout/Footer/index.tsx
+++ src/views/layout/Footer/index.tsx
@@ -3,23 +3,18 @@
  * @Author: Xcracker
  * @Date: 2022-10-13 22:36:54
  * @LastEditors: Xcracker
- * @LastEditTime: 2022-10-19 23:22:00
+ * @LastEditTime: 2022-11-20 22:31:27
  */
 import React from 'react';
 
-const loginPath: string = `${window.location.href}/login`;
 
 const Footer = () => {
     return (
         <nav className='xc-footer' id='bottomBar'>
-            <span className='algorithms'>
-                XCRACKERS
-                <span className='web-path'>.com</span>
+            <span className='copyright'>
+                Copyright © 2022-2023 xCracker. 当前呈现版本 1.0.0
             </span>
             <span className='login-button'>
-                <a href={loginPath} className='button-box'>
-                    LOGIN
-                </a>
             </span>
         </nav>
     );
